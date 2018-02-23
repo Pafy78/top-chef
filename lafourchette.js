2883ad2b23778e6f153f2130c9ae43f1f1f71461
Pafy78 <pierre.baudoin@devinci.fr>
2018-02-23 14:38:51 +0100

    add new request type

--- lafourchette.js
+++ lafourchette.js
@@ -26,17 +26,34 @@ exports.GetRestaurantByTitle = function (detail, callback) {
 
 exports.GetPromotion = function(callback){
     fs.readFile('lafourchette_details.json', 'utf8', function (err, data) {
+        console.log('\033[2J');
         console.log("0/" +   JSON.parse(data).length);
         if (err) throw err;
         var count = 0;
         for(var attributename in JSON.parse(data)){
-            request('https://www.lafourchette.com' + JSON.parse(data)[attributename].fourchette_url, function (error, response, body) {
+            //var url = 'https://www.lafourchette.com/reservation/module/date-list/' + JSON.parse(data)[attributename].fourchette_url.split("/")[3];
+            var url = 'https://www.lafourchette.com' + JSON.parse(data)[attributename].fourchette_url;
+            var configuration = {
+                'uri': url,
+                'headers': {
+                    'cookie': 'datadome=AHrlqAAAAAMAF4a7sY37iSUAVvJqHA=='
+                }
+            };
+            request(configuration, function (error, response, body) {
+                /*console.log(body.substring(174,206));
+                configuration = {
+                    'uri': url,
+                    'headers': {
+                        'cookie': 'datadome=' + body.substring(174,206)
+                    }
+                };*/
                 if (!error && response.statusCode == 200) {
                     count++;
+                    console.log('\033[2J');
                     console.log(count + "/" +   JSON.parse(data).length);
                     var $ = cheerio.load(body);
                     $('.moduleSaleType').filter(function(){
-                        console.log("Find restaurant !");
+                        console.log("Restaurant found !");
                         if(count == JSON.parse(data).length){
                             callback(JSON.parse(data)[attributename].fourchette_url, true);
                         }
